495740c81e7cebed36b7b51f63648d5e32faa479
Pengoose <pengoose@Pengooseui-MacBookPro.local>
2023-10-08 11:11:02 +0900

    chore: 기존 파일 제거

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Component';
-export * from './MovieList';
-export * from './MovieComponent';
--- src/constants/event.js
+++
@@ -1,4 +0,0 @@
-export const EVENT = Object.freeze({
-  LOADING_STATE_CHANGE: 'onLoadingStateChange',
-  CLICK: 'click',
-});
